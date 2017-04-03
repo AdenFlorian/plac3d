068d51c241f318d91d1af3782b9a357d35bede13
David Valachovic <AdenFlorian@gmail.com>
2017-04-03 12:53:31 -0500

    optimized vertices list

--- Assets/Scripts/CubeGen.cs
+++ Assets/Scripts/CubeGen.cs
@@ -5,10 +5,8 @@ public class CubeGen : MonoBehaviour
 	public int numberOfCubes;
 	public int rowNumber;
 
-    // Use this for initialization
     void Start()
     {
-        // You can change that line to provide another MeshFilter
         MeshFilter filter = gameObject.AddComponent<MeshFilter>();
         Mesh mesh = filter.mesh;
         mesh.Clear();
@@ -18,18 +16,13 @@ public class CubeGen : MonoBehaviour
         int height = 1;
 
         #region Vertices
-
-        var vertices = new Vector3[24 * numberOfCubes];
+		int numberOfVerticesPerCube = 8;
+        var vertices = new Vector3[numberOfVerticesPerCube * numberOfCubes];
 
 		for (int i = 0; i < numberOfCubes; i++)
 		{
-			//Debug.Log(i + " / " + canvasWidth + ": " + i / canvasWidth);
-
 			int hozMod = i - (numberOfCubes * (i / numberOfCubes));
 			int vertMod = (i / numberOfCubes) * length;
-
-			//Debug.Log("hozMod: " + hozMod);
-			//Debug.Log("vertMod: " + vertMod);
 
 			var p0 = new Vector3(-length * .5f + vertMod, -width * .5f, height * .5f + hozMod);
 			var p1 = new Vector3(length * .5f + vertMod, -width * .5f, height * .5f + hozMod);
@@ -41,150 +34,59 @@ public class CubeGen : MonoBehaviour
 			var p6 = new Vector3(length * .5f + vertMod, width * .5f, -height * .5f + hozMod);
 			var p7 = new Vector3(-length * .5f + vertMod, width * .5f, -height * .5f + hozMod);
 
-			vertices[(i * 24) + 0] = p0;
-			vertices[(i * 24) + 1] = p1;
-			vertices[(i * 24) + 2] = p2;
-			vertices[(i * 24) + 3] = p3;
-			vertices[(i * 24) + 4] = p7;
-			vertices[(i * 24) + 5] = p4;
-			vertices[(i * 24) + 6] = p0;
-			vertices[(i * 24) + 7] = p3;
-			vertices[(i * 24) + 8] = p4;
-			vertices[(i * 24) + 9] = p5;
-			vertices[(i * 24) + 10] = p1;
-			vertices[(i * 24) + 11] = p0;
-			vertices[(i * 24) + 12] = p6;
-			vertices[(i * 24) + 13] = p7;
-			vertices[(i * 24) + 14] = p3;
-			vertices[(i * 24) + 15] = p2;
-			vertices[(i * 24) + 16] = p5;
-			vertices[(i * 24) + 17] = p6;
-			vertices[(i * 24) + 18] = p2;
-			vertices[(i * 24) + 19] = p1;
-			vertices[(i * 24) + 20] = p7;
-			vertices[(i * 24) + 21] = p6;
-			vertices[(i * 24) + 22] = p5;
-			vertices[(i * 24) + 23] = p4;
-		}
+			vertices[(i * numberOfVerticesPerCube) + 0] = p0;
+			vertices[(i * numberOfVerticesPerCube) + 1] = p1;
+			vertices[(i * numberOfVerticesPerCube) + 2] = p2;
+			vertices[(i * numberOfVerticesPerCube) + 3] = p3;
+			vertices[(i * numberOfVerticesPerCube) + 4] = p4;
+			vertices[(i * numberOfVerticesPerCube) + 5] = p5;
+			vertices[(i * numberOfVerticesPerCube) + 6] = p6;
+			vertices[(i * numberOfVerticesPerCube) + 7] = p7;
+        }
         #endregion
 
-        // #region Normales
-        // var up = Vector3.up;
-        // var down = Vector3.down;
-        // var front = Vector3.forward;
-        // var back = Vector3.back;
-        // var left = Vector3.left;
-        // var right = Vector3.right;
-
-		// var normales = new Vector3[24 * numberOfCubes];
-
-		// for (int i = 0; i < numberOfCubes; i++)
-		// {
-		// 	normales[(i * 24) + 0] = down;
-		// 	normales[(i * 24) + 1] = down;
-		// 	normales[(i * 24) + 2] = down;
-		// 	normales[(i * 24) + 3] = down;
-		// 	normales[(i * 24) + 4] = left;
-		// 	normales[(i * 24) + 5] = left;
-		// 	normales[(i * 24) + 6] = left;
-		// 	normales[(i * 24) + 7] = left;
-		// 	normales[(i * 24) + 8] = front;
-		// 	normales[(i * 24) + 9] = front;
-		// 	normales[(i * 24) + 10] = front;
-		// 	normales[(i * 24) + 11] = front;
-		// 	normales[(i * 24) + 12] = back;
-		// 	normales[(i * 24) + 13] = back;
-		// 	normales[(i * 24) + 14] = back;
-		// 	normales[(i * 24) + 15] = back;
-		// 	normales[(i * 24) + 16] = right;
-		// 	normales[(i * 24) + 17] = right;
-		// 	normales[(i * 24) + 18] = right;
-		// 	normales[(i * 24) + 19] = right;
-		// 	normales[(i * 24) + 20] = up;
-		// 	normales[(i * 24) + 21] = up;
-		// 	normales[(i * 24) + 22] = up;
-		// 	normales[(i * 24) + 23] = up;
-		// }
-        // #endregion
-
-        // #region UVs
-        // var _00 = new Vector2(0f, 0f);
-        // var _10 = new Vector2(1f, 0f);
-        // var _01 = new Vector2(0f, 1f);
-        // var _11 = new Vector2(1f, 1f);
-
-        // var uvs = new Vector2[24 * numberOfCubes];
-
-		// for (int i = 0; i < numberOfCubes; i++)
-		// {
-		// 	uvs[(i * 24) + 0] = _11;
-		// 	uvs[(i * 24) + 1] = _01;
-		// 	uvs[(i * 24) + 2] = _00;
-		// 	uvs[(i * 24) + 3] = _10;
-		// 	uvs[(i * 24) + 4] = _11;
-		// 	uvs[(i * 24) + 5] = _01;
-		// 	uvs[(i * 24) + 6] = _00;
-		// 	uvs[(i * 24) + 7] = _10;
-		// 	uvs[(i * 24) + 8] = _11;
-		// 	uvs[(i * 24) + 9] = _01;
-		// 	uvs[(i * 24) + 10] = _00;
-		// 	uvs[(i * 24) + 11] = _10;
-		// 	uvs[(i * 24) + 12] = _11;
-		// 	uvs[(i * 24) + 13] = _01;
-		// 	uvs[(i * 24) + 14] = _00;
-		// 	uvs[(i * 24) + 15] = _10;
-		// 	uvs[(i * 24) + 16] = _11;
-		// 	uvs[(i * 24) + 17] = _01;
-		// 	uvs[(i * 24) + 18] = _00;
-		// 	uvs[(i * 24) + 19] = _10;
-		// 	uvs[(i * 24) + 20] = _11;
-		// 	uvs[(i * 24) + 21] = _01;
-		// 	uvs[(i * 24) + 22] = _00;
-		// 	uvs[(i * 24) + 23] = _10;
-		// }
-        // #endregion
-
         #region Triangles
-        int[] triangles = new int[36 * numberOfCubes];
+        int numberOfTrianglesPerCube = 12;
+        int[] triangles = new int[numberOfTrianglesPerCube * 3 * numberOfCubes];
 
 		for (int i = 0; i < numberOfCubes; i++)
 		{
-			triangles[(i * 36) + 0] = 3 + (i * 24);
-			triangles[(i * 36) + 1] = 1 + (i * 24);
-			triangles[(i * 36) + 2] = 0 + (i * 24);
-			triangles[(i * 36) + 3] = 3 + (i * 24);
-			triangles[(i * 36) + 4] = 2 + (i * 24);
-			triangles[(i * 36) + 5] = 1 + (i * 24);
-			triangles[(i * 36) + 6] = (3 + 4 * 1) + (i * 24);
-			triangles[(i * 36) + 7] = (1 + 4 * 1) + (i * 24);
-			triangles[(i * 36) + 8] = (0 + 4 * 1) + (i * 24);
-			triangles[(i * 36) + 9] = (3 + 4 * 1) + (i * 24);
-			triangles[(i * 36) + 10] = (2 + 4 * 1) + (i * 24);
-			triangles[(i * 36) + 11] = (1 + 4 * 1) + (i * 24);
-			triangles[(i * 36) + 12] = (3 + 4 * 2) + (i * 24);
-			triangles[(i * 36) + 13] = (1 + 4 * 2) + (i * 24);
-			triangles[(i * 36) + 14] = (0 + 4 * 2) + (i * 24);
-			triangles[(i * 36) + 15] = (3 + 4 * 2) + (i * 24);
-			triangles[(i * 36) + 16] = (2 + 4 * 2) + (i * 24);
-			triangles[(i * 36) + 17] = (1 + 4 * 2) + (i * 24);
-			triangles[(i * 36) + 18] = (3 + 4 * 3) + (i * 24);
-			triangles[(i * 36) + 19] = (1 + 4 * 3) + (i * 24);
-			triangles[(i * 36) + 20] = (0 + 4 * 3) + (i * 24);
-			triangles[(i * 36) + 21] = (3 + 4 * 3) + (i * 24);
-			triangles[(i * 36) + 22] = (2 + 4 * 3) + (i * 24);
-			triangles[(i * 36) + 23] = (1 + 4 * 3) + (i * 24);
-			triangles[(i * 36) + 24] = (3 + 4 * 4) + (i * 24);
-			triangles[(i * 36) + 25] = (1 + 4 * 4) + (i * 24);
-			triangles[(i * 36) + 26] = (0 + 4 * 4) + (i * 24);
-			triangles[(i * 36) + 27] = (3 + 4 * 4) + (i * 24);
-			triangles[(i * 36) + 28] = (2 + 4 * 4) + (i * 24);
-			triangles[(i * 36) + 29] = (1 + 4 * 4) + (i * 24);
-			triangles[(i * 36) + 30] = (3 + 4 * 5) + (i * 24);
-			triangles[(i * 36) + 31] = (1 + 4 * 5) + (i * 24);
-			triangles[(i * 36) + 32] = (0 + 4 * 5) + (i * 24);
-			triangles[(i * 36) + 33] = (3 + 4 * 5) + (i * 24);
-			triangles[(i * 36) + 34] = (2 + 4 * 5) + (i * 24);
-			triangles[(i * 36) + 35] = (1 + 4 * 5) + (i * 24);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 0] = 0 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 1] = 3 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 2] = 2 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 3] = 2 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 4] = 1 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 5] = 0 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 6] = 0 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 7] = 1 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 8] = 5 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 9] = 5 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 10] = 4 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 11] = 0 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 12] = 1 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 13] = 2 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 14] = 6 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 15] = 6 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 16] = 5 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 17] = 1 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 18] = 2 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 19] = 3 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 20] = 7 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 21] = 7 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 22] = 6 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 23] = 2 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 24] = 0 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 25] = 4 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 26] = 7 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 27] = 7 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 28] = 3 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 29] = 0 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 30] = 4 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 31] = 5 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 32] = 6 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 33] = 6 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 34] = 7 + (i * numberOfVerticesPerCube);
+			triangles[(i * numberOfTrianglesPerCube * 3) + 35] = 4 + (i * numberOfVerticesPerCube);
 		}
         #endregion
 
@@ -213,18 +115,10 @@ public class CubeGen : MonoBehaviour
         #endregion
 
         mesh.vertices = vertices;
-        //mesh.normals = normales;
-        //mesh.uv = uvs;
         mesh.triangles = triangles;
 		mesh.colors32 = colors;
 
         mesh.RecalculateBounds();
         mesh.Optimize();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
